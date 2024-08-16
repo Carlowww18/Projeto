32ebc26a1c3bab57c5b66615bacf4042b4ec64d7
Carlos Andre <carlowww38@gmail.com>
2024-08-16 19:57:03 -0300

    incompleto

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name="home"),
+    path('', views.home, name="home"),
+    path('receitas/<int:id>/', views.receitas, name="receitas"),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from . utils.receitas.factory import make_recipe
 
 def home(request):
-    return render(request, 'receitas/pages/home.html', {'name': 'André'})
+    return render(request, 'receitas/pages/home.html', {'recipes': [make_recipe() for _ in range(3)]},)
+
+def receitas(request, id):
+    return render(request, 'receitas/pages/receitas-views.html', {'recipe': make_recipe(),
+                                                                  'is_detail_page': True,
+                                                                  })
