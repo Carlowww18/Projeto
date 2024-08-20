3f6016c68b073961d63c2b863b446a87b0995a8f
Carlos Andre <carlowww38@gmail.com>
2024-08-20 16:43:23 -0300

    atualizando

--- receitas/tests.py
+++ receitas/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from receitas import views
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_category_home_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 6})
+        self.assertEqual(url, '/receitas/category/6/')
+
+    def test_receita_home_url_is_correct(self):
+        url = reverse('recipes:receitas', kwargs={'id': 1})
+        self.assertEqual(url, '/receitas/1/')
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 6}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_function_is_correct(self):  
+        view = resolve(reverse('recipes:receitas', kwargs={'id': 1}))
+        self.assertIs(view.func, views.receitas)
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
-from django.shortcuts import render
 from . models import Recipe
 
 def home(request):
