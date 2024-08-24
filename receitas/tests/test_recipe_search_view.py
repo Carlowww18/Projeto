041e3a94602262d51f60b2d5a7164e623b2888c1
Carlos Andre <carlowww38@gmail.com>
2024-08-24 15:19:52 -0300

    atualizando

--- receitas/tests/test_recipe_search_view.py
+++ receitas/tests/test_recipe_search_view.py
@@ -4,7 +4,83 @@ from receitas import views
 from receitas.models import Category, Recipe, User
 
 
-class RecipeSearchViewTes(TestCase):
+class RecipeMixin:
+    def make_category(self, name='Category'):
+        return Category.objects.create(name=name)
+
+    def make_author(
+        self,
+        first_name='user',
+        last_name='name',
+        username='username',
+        password='123456',
+        email='username@email.com',
+    ):
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
+        )
+
+    def make_recipe(
+        self,
+        category_data=None,
+        author_data=None,
+        title='Recipe Title',
+        description='Recipe Description',
+        slug='recipe-slug',
+        preparation_time=10,
+        preparation_time_unit='Minutos',
+        servings=5,
+        servings_unit='Porções',
+        preparation_steps='Recipe Preparation Steps',
+        preparation_steps_is_html=False,
+        is_published=True,
+    ):
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(
+            category=self.make_category(**category_data),
+            author=self.make_author(**author_data),
+            title=title,
+            description=description,
+            slug=slug,
+            preparation_time=preparation_time,
+            preparation_time_unit=preparation_time_unit,
+            servings=servings,
+            servings_unit=servings_unit,
+            preparation_steps=preparation_steps,
+            preparation_steps_is_html=preparation_steps_is_html,
+            is_published=is_published,
+        )
+
+    def make_recipe_in_batch(self, qtd=10):
+        recipes = []
+        for i in range(qtd):
+            kwargs = {
+                'title': f'Recipe Title {i}',
+                'slug': f'r{i}',
+                'author_data': {'username': f'u{i}'}
+            }
+            recipe = self.make_recipe(**kwargs)
+            recipes.append(recipe)
+        return recipes
+
+
+class RecipeTestBase(TestCase, RecipeMixin):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+
+    
+
+class RecipeSearchViewTest(TestCase, RecipeMixin):
 
     def test_recipe_search_users_correct_view_function(self):
         url = reverse('recipes:search')
@@ -23,3 +99,26 @@ class RecipeSearchViewTes(TestCase):
         url = reverse('recipes:search') + '?q=<Teste>'
         response = self.client.get(url)
         self.assertIn('Search for &quot;&lt;Teste&gt;&quot;', response.content.decode('utf-8'))
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is recipe one'
+        title2 = 'This is recipe two'
+
+        recipe1 = self.make_recipe(
+            slug='one',
+            title=title1,
+            author_data={ 'username': 'one' }
+        )
+
+        recipe2 = self.make_recipe(
+            slug='two',
+            title=title2,
+            author_data={ 'username': 'two' }
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url} + ?q={title1}')
+        response2 = self.client.get(f'{search_url} + ?q={title2}')
+        response_both = self.client.get(f'{search_url} + ?q=this')
+        pass
+
--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse, Http404
 from django.db.models import Q
+from django.core.paginator import Paginator
+from receitas.utils.pagination import make_pagination_range
 from . models import Recipe
 
 def home(request):
     recipes = Recipe.objects.all().order_by('-id')
-    return render(request, 'receitas/pages/home.html', {'recipes': recipes},)
+
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 6)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
+
+    return render(request, 'receitas/pages/home.html', {
+        'recipes': page_obj, 
+        'pagination_range': pagination_range
+    })
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id = category_id).order_by('-id')
