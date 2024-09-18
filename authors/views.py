a1b1663ddcd38a96cccbf2923f8dd9c24350ed59
Carlos Andre <carlowww38@gmail.com>
2024-09-18 15:54:12 -0300

    mudando de modulo

--- authors/views.py
+++ authors/views.py
@@ -3,6 +3,8 @@ from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def register_view(request):
@@ -25,6 +27,7 @@ def register_create(request):
         messages.success(request, 'Your user is created, please log in.')
 
         del(request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     else:
         messages.info(request, 'Corrija os campos inválidos')
@@ -37,4 +40,27 @@ def login_view(request):
                                                             'form_action': reverse('authors:login_create')})
 
 def login_create(request):
-    return render(request, 'author/pages/login_view.html')
+    if not request.POST:
+        raise Http404()
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+    
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username = form.cleaned_data.get('username', ''),
+            password = form.cleaned_data.get('password', ''),
+        )
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials.')
+    else:
+        messages.error(request, 'Error to validate form data')
+
+    return redirect(login_url)  
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('authors:login'))
