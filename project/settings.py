30c8935777afcf82c64999b252bda173cdba6b5d
Carlos Andre <carlowww38@gmail.com>
2024-11-01 10:39:28 -0300

    atualizando

--- project/settings.py
+++ project/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
-ALLOWED_HOSTS = parse_comma_str_to_list(get_env_variable('ALLOWED_HOSTS'))
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['*'] =  parse_comma_str_to_list(get_env_variable('ALLOWED_HOSTS'))
+
 
 # Application definition
 
