38eedd5901f6d1c026b24e0b893954959f080002
Carlos Andre <carlowww38@gmail.com>
2024-11-14 11:17:11 -0300

    teste

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = parse_comma_str_to_list(
-    get_env_variable('ALLOWED_HOST')
+    get_env_variable('ALLOWED_HOSTS')
 )
 
 
